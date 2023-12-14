679a42d04ed980d7c66e4ef21c1ff944f157d8a2
gautam rishi <gautam.rishi@tabcorp.com.au>
2023-12-14 15:49:03 +0530

    friends api created

--- api/serializers.py
+++ api/serializers.py
@@ -4,14 +4,19 @@ from .helper import convert_epoch_to_datetime
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = '__all__'
+        fields = ['id', 'name', 'email','avatar', 'timestamp']
     
     def to_representation(self, instance):
         representation = super(CustomerSerializer, self).to_representation(instance)
-        representation['password'] = '*'*len(representation['password'])
+        # representation['password'] = '*'*len(representation['password'])
         representation['timestamp'] = convert_epoch_to_datetime(representation['timestamp'])
         return representation
 
+
+class FriendsSerlializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['id','name']
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,8 +26,23 @@ class GroupSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         representation = super(GroupSerializer, self).to_representation(instance)
         representation['timestamp'] = convert_epoch_to_datetime(representation['timestamp'])
+        FriendsData = CustomerSerializer(instance.customers, many=True).data
+        
+        friends_list = []
+        for friend in FriendsData:
+            if self.context.get('user_id') != friend['id']: 
+                friends_list.append(friend['name']) 
+        
+        representation['customers'] = friends_list
+        # need to put balance amount for each friend later
+        # representation['settlements'] = 
         return representation
-
+    
+class GroupFriendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+    
 class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
